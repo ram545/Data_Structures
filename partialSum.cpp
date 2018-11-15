9cd8f43c96cdaa9a16dffe9e451fce71ef4d4a95
Rama Krishna Reddy Sabbella <20751687+ram545@users.noreply.github.com>
2018-11-15 12:32:50 +0530

    Update partialSum.cpp

--- partialSum.cpp
+++ partialSum.cpp
@@ -1,16 +1,28 @@
 /*  Given a sequence of elements and asked about the sum of part of its elements(without any modifying queries)
-*   Given range l,r (l!=r) and (1 <= l < r <= N)
-*   Algorithm:
+*  Algorithm:
 *   1. Build an array in such a way that a[i] contains sum of all elements from 0 to i.
 *   2. So for a given range(l,r) sum of parts = a[r] - a[l] 
+*  Input :
+*   First line contains number of queries and elements in sequence
+*   Second line contains elements of the sequence
+*   Subsequent line contains l,r(1<= l < r <= N)
+*   5 6
+*   1 2 3 4 5 6
+*   1 6 
+*   2 4 
+*   2 3 
+*   3 4 
+*   3 6 
+*  Output :
+*   print respective sum after each query
 */
 
 #include<iostream>
 #include<cstring>
 
 int main(){
-    int N,*Arr,*sumArr,testCase,i,l,r;
-    std::cin >> testCase;
+    int N,*Arr,*sumArr,query,i,l,r;
+    std::cin >> query;
     std::cin >> N;
     Arr = new int[N];
     sumArr = new int[N];
@@ -21,9 +33,8 @@ int main(){
             sumArr[i] = Arr[i];
         else
             sumArr[i] = Arr[i] + sumArr[i-1];
-        std::cout << sumArr[i] << " " << i << std::endl;
     }
-    while(testCase--){
+    while(query--){
         std::cin >> l >> r;
         if( (l == 1 && r == N) || l==1)
             std::cout << sumArr[r-1] << std::endl;
