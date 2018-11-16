b68049a3b9a94976f0957155ee837da233f86c2d
Rama Krishna Reddy Sabbella <20751687+ram545@users.noreply.github.com>
2018-11-16 16:13:44 +0530

    Update troynacciQuery.cpp

--- troynacciQuery.cpp
+++ troynacciQuery.cpp
@@ -17,9 +17,6 @@ int main(){
     p[i+1] = 0;
     for(i=2;i<N+2;i++)
         series[i] = (1LL*a*series[i-2] + 1LL*b*series[i-1])%mod;
-    for(i=0;i<N;i++)
-            std::cout << series[i] << " ";
-        std::cout << std::endl;
     while(query--){
         std::cin >> l >> r;
         l--;
@@ -27,28 +24,41 @@ int main(){
         if(l<r){
             p[l] = (p[l]+series[0])%mod;
             p[l+1] = (p[l+1] + series[1])%mod;
-            p[l+1] = (1LL*p[l+1] - ((1LL*b*series[1])%mod))%mod;
-            p[r+1] = (1LL*p[r+1] - series[r-l+1])%mod;
-            p[r+2] = (1LL*p[r+1] - ((1LL*a*series[r-l])%mod))%mod;
+            p[l+1] = (1LL*p[l+1] + mod - ((1LL*b*series[1])%mod))%mod;
+            p[r+1] = (1LL*p[r+1] + mod - series[r-l+1])%mod;
+            p[r+2] = (1LL*p[r+2] + mod - ((1LL*a*series[r-l])%mod))%mod;
         }
         else{
             p[l] = (p[l]+series[0])%mod;
-            p[r+1] = (1LL*p[r+1] - ((1LL*b*series[0])%mod))%mod;
-            p[r+2] = (1LL*p[r+2] - ((1LL*a*series[0])%mod))%mod;
+            p[r+1] = (1LL*p[r+1] + mod - ((1LL*b*series[0])%mod))%mod;
+            p[r+2] = (1LL*p[r+2] + mod - ((1LL*a*series[0])%mod))%mod;
         }
-        for(i=0;i<N+2;i++)
-            std::cout << p[i] << " ";
-        std::cout << std::endl;
     }
+    Arr[0] = (Arr[0]+p[0])%mod;
     for(i=1;i<N;i++){
         if(i==1)
             p[i] = (1LL*p[i] + (1LL*a*p[i-1])%mod)%mod;
         else
-            p[i] = ((1LL*p[i-2]*a)%mod + (1LL*b*p[i-1])%mod)%mod;
+            p[i] = ((1LL*p[i-2]*a)%mod + (1LL*b*p[i-1])%mod + 1LL*p[i])%mod;
+        Arr[i] = (p[i]+Arr[i])%mod;
     }
     for(i=0;i<N;i++)
-        std::cout << Arr[i]+p[i] << " ";
+        std::cout << Arr[i] << " ";
     std::cout << std::endl;
 }
+
+/*
+6 6
+1 1
+1 1
+0 0 0 0 0 0
+1 6
+1 1
+4 5
+2 2
+4 4
+5 6
+*/
+
 
 
